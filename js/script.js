6dafb883b6280bcf8cbf28ddf3b56431bc6e5fa2
azillauri25 <savzans@gmail.com>
2023-12-13 18:59:54 +0700

    final webgis

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// const hideMarkerLabel = document.getElementById('hide-marker-label');
+//   const showMarkerLabel = document.getElementById('show-marker-label');
+
+//   // Menambahkan event listener untuk merespon klik pada label hide marker
+//   hideMarkerLabel.addEventListener('click', function() {
+//     // Menonaktifkan class active pada show marker
+//     showMarkerLabel.classList.remove('active');
+//     // Menambahkan class active pada hide marker
+//     hideMarkerLabel.classList.add('active');
+//   });
+
+//   // Menambahkan event listener untuk merespon klik pada label show marker
+//   showMarkerLabel.addEventListener('click', function() {
+//     // Menonaktifkan class active pada hide marker
+//     hideMarkerLabel.classList.remove('active');
+//     // Menambahkan class active pada show marker
+//     showMarkerLabel.classList.add('active');
+//   });
